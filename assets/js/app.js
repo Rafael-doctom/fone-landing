a1ac5226948955e537d41b75b4708090a42ded42
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-06-06 17:00:45 -0300

    feat: input being rendered on click

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+let searchIcon = document.getElementById('searchIcon');
+let input = document.getElementById("input");
+
+searchIcon.addEventListener('click', () => {
+
+    if (input.style.display === "none") {
+        input.style.display = "block";
+    } else {
+        input.style.display = "block";
+    };
+
+});
